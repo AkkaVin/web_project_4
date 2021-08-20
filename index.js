5dcbf958f5bda60d736afd7423485223627730ec
Akkavin <vinarski.a@gmail.com>
2021-08-20 17:47:27 +0300

    popup o/c 1 step

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // alert("Hello word!")
+const editProfileButton = document.querySelector(".profile__edit-btn");
+const popup  = document.querySelector(".popup");
+const popupCloseButton = popup.querySelector(".popup__close-btn");
+
+
+console.log (editProfileButton);
+console.log (popup);
+console.log (popupCloseButton);
+
+
+editProfileButton.addEventListener ('click',function (){
+  popup.classList.remove("popup_hide");
+})
+
+popupCloseButton.addEventListener ('click',function (){
+  popup.classList.add("popup_hide");
+})
