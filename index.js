82a2e191f8fc29cecb6a670a518aa5afbb52eca9
Akkavin <vinarski.a@gmail.com>
2021-08-21 00:28:55 +0300

    popup o/c + form init 2 step ready to merge

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // alert("Hello word!")
 const editProfileButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileProfessions = document.querySelector(".profile__professions");
+
 const popup  = document.querySelector(".popup");
 const popupCloseButton = popup.querySelector(".popup__close-btn");
 
+const form = popup.querySelector(".form");
+const formName = form.querySelector("input[name='name']");
+const formProfessions = form.querySelector("input[name='professions']");
 
-console.log (editProfileButton);
-console.log (popup);
-console.log (popupCloseButton);
 
 
 editProfileButton.addEventListener ('click',function (){
+  formName.value = profileName.textContent;
+  formProfessions.value = profileProfessions.textContent;
+
   popup.classList.remove("popup_hide");
 })
 
 popupCloseButton.addEventListener ('click',function (){
   popup.classList.add("popup_hide");
 })
+
+
+
+
