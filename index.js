3e45027ef733bf13e2dc4f7ef9e72f981bb8a8f1
Akkavin <vinarski.a@gmail.com>
2021-08-30 16:34:35 +0300

    ready

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-let editProfileButton = document.querySelector(".profile__edit-btn");
-let addCardButton = document.querySelector(".profile__add-card-btn");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
+/* popup ----------------------------------------------------- */
 
-let popup = document.querySelector(".popup");
-let popupCloseButton = popup.querySelector(".popup__close-btn");
+function togglePopup ()
+{
+  if (popup.classList.contains("popup_hide")) {
+    formName.value = profileName.textContent;
+    formJob.value = profileJob.textContent;
 
-let form = popup.querySelector(".form");
-let formName = form.querySelector("input[name='name']");
-let formJob = form.querySelector("input[name='job']");
-let formSaveButton = form.querySelector(".form__save-btn");
+  }
+  popup.classList.toggle("popup_hide");
 
-let cardsContainer = document.querySelector(".cards__container");
-let cardTemplate = document.querySelector(".card-template").content;
-/*
+}
+
+/*--profile edit form ----------------------------------------------------------*/
+
+function saveForm(e) {
+  e.preventDefault();
+
+  profileName.textContent = formName.value;
+  profileJob.textContent = formJob.value;
+  togglePopup();
+}
+
+/* --cards-------------------------------------------------------------------*/
+
+function addOneCard(card) {
+  let newCard = cardTemplate.cloneNode(true);
+
+  newCard
+    .querySelector(".card__remove-btn")
+    .addEventListener("click", (evt) =>
+      evt.currentTarget.parentElement.remove()
+    );
+  let newCardImage = newCard.querySelector(".card__image");
+  newCardImage.src = card.link;
+  newCardImage.addEventListener("click", evt =>
+    alert("TODO fullscreen card image")
+  );
+  newCard.querySelector(".card__title").textContent = card.name;
+  newCard
+    .querySelector(".card__like-btn")
+    .addEventListener("click", (evt) =>
+      evt.currentTarget.classList.toggle("card__like-btn_active")
+    );
+
+  cardsContainer.prepend(newCard);
+}
+
+/* -----------------------------------------*/
+
+/* // init data
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -43,51 +78,36 @@ const initialCards = [
 
 */
 
-function showPopup() {
-  formName.value = profileName.textContent;
-  formJob.value = profileJob.textContent;
-  popup.classList.remove("popup_hide");
-}
+// profile
+// -- buttons
+let editProfileButton = document.querySelector(".profile__edit-btn");
+let addCardButton = document.querySelector(".profile__add-card-btn");
+// -- inputs
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
 
-function hidePopup() {
-  popup.classList.add("popup_hide");
-}
+// cards
+let cardsContainer = document.querySelector(".cards__container");
+let cardTemplate = document.querySelector(".card-template").content;
 
-function saveForm(e) {
-  e.preventDefault();
+// popup
+let popup = document.querySelector(".popup");
+// -- buttons
+let popupCloseButton = popup.querySelector(".popup__close-btn");
 
-  profileName.textContent = formName.value;
-  profileJob.textContent = formJob.value;
-  hidePopup();
-}
+// edit_profile_form
+let form = popup.querySelector(".form");
+// -- buttons
+let formSaveButton = form.querySelector(".form__save-btn");
+// -- inputs
+let formName = form.querySelector(".form__input_type_name");
+let formJob = form.querySelector(".form__input_type_job");
 
-function addOneCard(card) {
-  let newCard = cardTemplate.cloneNode(true);
 
-  newCard
-    .querySelector(".card__remove-btn")
-    .addEventListener("click", (evt) =>
-      evt.currentTarget.parentElement.remove()
-    );
-  let newCardImage = newCard.querySelector(".card__image");
-  newCardImage.src = card.link;
-  newCardImage.addEventListener("click", evt =>
-    alert("TODO fullscreen card image")
-  );
-  newCard.querySelector(".card__title").textContent = card.name;
-  newCard
-    .querySelector(".card__like-btn")
-    .addEventListener("click", (evt) =>
-      evt.currentTarget.classList.toggle("card__like-btn_active")
-    );
-
-  cardsContainer.prepend(newCard);
-}
-
-editProfileButton.addEventListener("click", showPopup);
-popupCloseButton.addEventListener("click", hidePopup);
+// all listeners
+editProfileButton.addEventListener("click", togglePopup);
+popupCloseButton.addEventListener("click", togglePopup);
 addCardButton.addEventListener("click", function (evt) {
-  // alert("1");
   const card = {
     name: "Yosemite Valley",
     // link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
@@ -95,8 +115,4 @@ addCardButton.addEventListener("click", function (evt) {
   };
   addOneCard(card);
 });
-
 form.addEventListener("submit", saveForm);
-
-// let cardTemplate = document.querySelector(".card-template").content;
-console.log({ cardTemplate }, cardTemplate);
