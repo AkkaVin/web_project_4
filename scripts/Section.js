c6c0e45fa6f5db955183db0c14e98d3dadc45682
Akkavin <vinarski.a@gmail.com>
2022-03-16 00:50:04 +0200

    beta build with no full webpack and old file structute

---
+++ scripts/Section.js
@@ -0,0 +1,23 @@
+export default class Section {
+  constructor ({items, renderer}, elementContainerSelector)  {
+    this._items = items;
+    this._renderer = renderer;
+    this._elementContainer = document.querySelector(elementContainerSelector);
+  }
+
+  _clearContainer() {
+    this._elementContainer.innerHTML = "";
+  }
+
+  renderItems(){
+    _clearContainer();
+
+    this._items.forEach ( item => {
+      this._renderer(item)
+    })
+  }
+
+  addItem (element) {
+    this._elementContainer.append(element);
+  }
+}
