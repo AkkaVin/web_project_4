45d364c548c120b4b8829ace41d065a2da96358d
Akkavin <vinarski.a@gmail.com>
2022-03-25 01:12:30 +0200

    fixed cmpnt - PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,16 +3,16 @@ import {Popup} from "./Popup.js"
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+
+    this._popupImage = this._popupElement.querySelector(".popup__image");
+    this._popupImageTitle = this._popupElement.querySelector(".popup__image-title");
   }
 
   open ({text, src, alt}) {
 
-    const popupImage = this._popupElement.querySelector(".popup__image");
-    const popupImageTitle = this._popupElement.querySelector(".popup__image-title");
-
-    popupImage.src = src
-    popupImage.alt = alt;
-    popupImageTitle.textContent = text;
+    this._popupImage.src = src
+    this._popupImage.alt = alt;
+    this._popupImageTitle.textContent = text;
 
     super.open();
   }
