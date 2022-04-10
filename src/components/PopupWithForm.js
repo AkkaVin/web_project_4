cc2f55b6e966738db79af2e7e47254239e116c08
Akkavin <vinarski.a@gmail.com>
2022-04-11 00:00:13 +0300

    PopupwithFotm PopupWithSubmit methods renaming

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,9 +1,9 @@
 import { Popup } from "./Popup.js";
 
 export class PopupWithForm extends Popup {
-  constructor (popupSelector, submitHandler) {
+  constructor (popupSelector, handleSubmit) {
     super(popupSelector);
-    this._submitHandler = submitHandler;
+    this._handleSubmit = handleSubmit;
     this._popupForm = this._popupElement.querySelector(".form");
     this._inputList = this._popupForm.querySelectorAll(".form__input");
     this._submitButton = this._popupForm.querySelector(".form__save-btn");
@@ -32,7 +32,7 @@ export class PopupWithForm extends Popup {
     super.setEventListeners();
     this._popupForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      this._submitHandler(this._getInputValues())
+      this._handleSubmit(this._getInputValues())
     }
     // , { once: true}  // use it if need delete after event fire once and need set new listener
     );
--- src/components/popupWithSubmit.js
+++ src/components/popupWithSubmit.js
@@ -2,23 +2,20 @@ import {Popup} from "./Popup.js"
 
 export class PopupWithSubmit extends Popup {
 
-  constructor (popupWithSubmitSelector
-    // , submitHandler
-    ){
+  constructor (popupWithSubmitSelector) {
     super(popupWithSubmitSelector);
     this._popupForm = this._popupElement.querySelector(".form");
-    // this._submitHandler = submitHandler;
   }
 
   setAction(action) {
-    this._submitHandler = action;
+    this._handleSubmit = action;
   }
 
   setEventListeners() {
     super.setEventListeners();
     this._popupForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      this._submitHandler()
+      this._handleSubmit()
     });
   }
 
